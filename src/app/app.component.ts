638583d379827092363f1961eb35af3cfbf4716b
deepesh <deepesh554.dg@gmail.com>
2021-10-04 21:15:29 +0530

    changed prefix from app to dg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'dg-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
