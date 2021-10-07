6d3cad4d50964d420376e20f9b24f7ad7c57350a
Deepesh Gupta <dgupta@slt.work>
2021-10-07 13:18:41 +0530

    made components and services for http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,21 @@ import { FormsComponent } from './forms/forms.component';
 import { Form1Component } from './forms/form1/form1.component';
 import { Form2Component } from './forms/form2/form2.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpComponent } from './http/http.component';
+import { GetComponent } from './http/get/get.component';
+import { PostComponent } from './http/post/post.component';
+import { PutComponent } from './http/put/put.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormsComponent,
     Form1Component,
-    Form2Component
+    Form2Component,
+    HttpComponent,
+    GetComponent,
+    PostComponent,
+    PutComponent,
   ],
   imports: [
     BrowserModule,
