a69bc9f1b8036781c5416bf0624f7dfb30894402
Deepesh Gupta <dgupta@slt.work>
2021-10-11 12:09:02 +0530

    worked on services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -30,6 +31,7 @@ import { PutComponent } from './http/put/put.component';
     NgbModule,
     FormsModule,
     ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/http/service/api.service.ts
+++ src/app/http/service/api.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+
+export interface PostType { 
+  userId: number, 
+  id: number, 
+  title: string, 
+  body: string 
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  url: string = 'https://jsonplaceholder.typicode.com/';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getData(): Observable<PostType[]> {
+    return this.http.get<PostType[]>(this.url + 'posts');
+  }
 }
